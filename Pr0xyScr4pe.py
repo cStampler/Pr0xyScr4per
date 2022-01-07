28d1765a94204b951c4b3852080e04cb7d0ef379
David <71495733+cStampler@users.noreply.github.com>
2022-01-07 14:44:05 +0100

    Update Pr0xyScr4pe.py
    added multiple types of other proxies

--- Pr0xyScr4pe.py
+++ Pr0xyScr4pe.py
@@ -25,15 +25,12 @@ info = '''
 ║Dsicord: stampler#9931 for contact           ║
 ╚═════════════════════════════════════════════╝
 '''
-
-
-
 print(Fore.RED+info)
 print(Fore.CYAN+icon)
 
 
 print()
-ready = input(f'{Fore.YELLOW} This will generate 1k of each proxy type. (Hit "ENTER" To Continue) ')
+ready = input(f'{Fore.YELLOW} This will generate roughly 1k or more of each proxy type. (Hit "ENTER" To Continue) ')
 
 url1 = 'https://api.openproxylist.xyz/http.txt'
 url2 = 'https://www.proxy-list.download/api/v1/get?type=http'
@@ -42,17 +39,56 @@ r2 = requests.get(url2)
 results1 = r1.text
 results2 = r2.text
 with open ("http.txt", "w") as file:
- file.write(results1)
- file.write(results2)
+ file.write(results1 + results2)
+
+
+
+url1 = 'https://api.openproxylist.xyz/https.txt'
+url2 = 'https://www.proxy-list.download/api/v1/get?type=https'
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+results1 = r1.text
+results2 = r2.text
+with open ("https.txt", "w") as file:
+ file.write(results1 + results2)
+
+
+
+url1 = 'https://api.openproxylist.xyz/anon.txt'
+url2 = 'https://www.proxy-list.download/api/v1/get?type=https&anon=elite'
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+results1 = r1.text
+results2 = r2.text
+with open ("elitehttps.txt", "w") as file:
+ file.write(results1 + results2)
+
+
+url1 = 'https://api.openproxylist.xyz/anon.txt'
+url2 = 'https://www.proxy-list.download/api/v1/get?type=https&anon=elite&country=US'
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+results1 = r1.text
+results2 = r2.text
+with open ("eliteusahttps.txt", "w") as file:
+ file.write(results1 + results2)
+
+
  
-url = 'https://api.openproxylist.xyz/socks4.txt'
-r = requests.get(url)
-results = r.text
+url1 = 'https://api.openproxylist.xyz/socks4.txt'
+url2 = 'https://www.proxy-list.download/api/v1/get?type=socks4'
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+results1 = r1.text
+results2 = r2.text
 with open ("socks4.txt", "w") as file:
- file.write(results)
+ file.write(results1 + results2)
  
-url = 'https://api.openproxylist.xyz/socks5.txt'
-r = requests.get(url)
-results = r.text
+url1 = 'https://api.openproxylist.xyz/socks5.txt'
+url2 = 'https://www.proxy-list.download/api/v1/get?type=socks5'
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+results1 = r1.text
+results2 = r2.text
 with open ("socks5.txt", "w") as file:
- file.write(results)
+ file.write(results1 + results2)
